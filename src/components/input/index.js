97155237700446b4ed6b4ee9ddff6477d2a1e3cb
mihalay123 <mihalay26@gmail.com>
2022-04-06 19:40:05 +0500

    input propName fix

--- src/components/input/index.js
+++ src/components/input/index.js
@@ -77,7 +77,7 @@ const Input = forwardRef(
           onClick={onClick}
           value={value}
           ref={ref}
-          maxlength={maxLength}
+          maxLength={maxLength}
         />
 
         {icon ? (
